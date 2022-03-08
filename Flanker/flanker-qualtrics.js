ff72a99c39d7f20021e5f972603b7ead024b5b68
dvitaa <dkapadi1@swarthmore.edu>
2022-03-07 18:25:48 -0800

    001
    debrief return empty

--- Flanker/flanker-qualtrics.js
+++ Flanker/flanker-qualtrics.js
@@ -11,14 +11,14 @@ Qualtrics.SurveyEngine.addOnload(function () {
 
     /* Change 2: Defining and load required resources */
     // https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/<experiment-folder>
-    var task_github = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/";
+    var task_github = "https://cdn.jsdelivr.net/gh/dvitaa/flanker/Flanker/";
 
     // requiredResources must include all the JS files that demo-simple-rt-task-transformed.html uses.
     var requiredResources = [
         task_github + "jspsych-6.1.0/jspsych.js",
         task_github + "jspsych-6.1.0/plugins/jspsych-html-keyboard-response.js",
         task_github + "jspsych-6.1.0/plugins/jspsych-image-keyboard-response.js",
-        task_github + "flanker_main.js"
+        task_github + "flanker.js"
     ];
 
     function loadScript(idx) {
